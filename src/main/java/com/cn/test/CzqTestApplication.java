de828f6ae7f29ffa2aa107d5db530e00d16ec389
czqcui <czqcui@cn.ibm.com>
2025-06-17 14:50:17 +0800

    2

---
+++ src/main/java/com/cn/test/CzqTestApplication.java
@@ -0,0 +1,13 @@
+package com.cn.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CzqTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CzqTestApplication.class, args);
+	}
+
+}
